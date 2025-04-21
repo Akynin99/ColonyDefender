76f5dac593360628a540e50838e236a7b3733bdd
Stepan Ustinov <Akynin99@gmail.com>
2025-04-21 23:02:07 +0700

    feat: extend the functionality of ResourceService

--- Assets/Scripts/Core/Interfaces/IResourceService.cs
+++ Assets/Scripts/Core/Interfaces/IResourceService.cs
@@ -1,8 +1,42 @@
-﻿namespace ColonyDefender.Core
+﻿using System;
+
+namespace ColonyDefender.Core
 {
     public interface IResourceService
     {
+        /// <summary>
+        /// Tries to spend energy if there's enough
+        /// </summary>
         bool TrySpendEnergy(int amount);
+        
+        /// <summary>
+        /// Tries to spend minerals if there's enough
+        /// </summary>
+        bool TrySpendMinerals(int amount);
+        
+        /// <summary>
+        /// Tries to spend both energy and minerals if there's enough of both
+        /// </summary>
+        bool TrySpendResources(int energyAmount, int mineralAmount);
+        
+        /// <summary>
+        /// Checks if there are enough resources
+        /// </summary>
+        bool HasEnoughResources(int energyAmount, int mineralAmount);
+        
+        /// <summary>
+        /// Adds energy to the colony
+        /// </summary>
+        void AddEnergy(int amount);
+        
+        /// <summary>
+        /// Adds minerals to the colony
+        /// </summary>
         void AddMinerals(int amount);
+        
+        /// <summary>
+        /// Updates resources based on production rates
+        /// </summary>
+        void UpdateResourceProduction();
     }
 }
