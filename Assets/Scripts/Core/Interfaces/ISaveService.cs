8f9d86c94b916a19f15ef2a1b63ac168f3616ec2
Stepan Ustinov <Akynin99@gmail.com>
2025-04-21 14:35:52 +0700

    feat: add ISaveService interface

---
+++ Assets/Scripts/Core/Interfaces/ISaveService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UniRx;
+
+namespace ColonyDefender.Core
+{
+    public interface ISaveService
+    {
+        /// <summary>
+        /// Save colony data async
+        /// </summary>
+        UniTask SaveColonyDataAsync(ColonyData data, CancellationToken token);
+        
+        /// <summary>
+        /// Loads colony data with a reactive result
+        /// </summary>
+        IUniTaskAsyncEnumerable<ColonyData> LoadColonyDataAsync(CancellationToken token);
+        
+        /// <summary>
+        /// Reactive property with last save time
+        /// </summary>
+        IReadOnlyReactiveProperty<System.DateTime?> LastSaveTime { get; }
+    }
+}
