3dcfb3e6ac394aefb719177386d9716bc2adba6c
Stepan Ustinov <Akynin99@gmail.com>
2025-04-21 12:11:16 +0700

    feat: add wave spawner

---
+++ Assets/Scripts/IWaveManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+
+namespace ColonyDefender
+{
+    public interface IWaveManager
+    {
+        UniTaskVoid StartWaveAsync(WaveConfig wave, CancellationToken token);
+    }
+}
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace ColonyDefender
+{
+    public class WaveSpawner : IWaveManager
+    {
+        public async UniTaskVoid StartWaveAsync(WaveConfig wave, CancellationToken token)
+        {
+            UniTask.Void(async () =>
+            {
+                foreach (var enemy in wave.Enemies)
+                {
+                    await UniTask.Delay(
+                        TimeSpan.FromSeconds(wave.SpawnInterval),
+                        cancellationToken: token
+                    );
+
+                    if (token.IsCancellationRequested)
+                        return;
+
+                    InstantiateEnemy(enemy);
+                }
+            });
+        }
+
+        private void InstantiateEnemy(GameObject prefab)
+        {
+            // TODO: object pooling
+
+        }
+    }
+}
