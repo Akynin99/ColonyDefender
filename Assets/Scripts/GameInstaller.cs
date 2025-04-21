e573ef4cfd414c3c4162df04490322a7b16ef058
Stepan Ustinov <Akynin99@gmail.com>
2025-04-21 12:27:36 +0700

    feat: add game installer

---
+++ Assets/Scripts/GameInstaller.cs
@@ -0,0 +1,31 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace ColonyDefender
+{
+    public class GameInstaller : MonoBehaviour
+    {
+        [SerializeField] private WaveConfig initialWave;
+    
+        private ColonyData _data;
+        private IResourceService _resources;
+        private ColonyViewModel _viewModel;
+
+        private void Start()
+        {
+            _data = new ColonyData();
+            _resources = new ResourceService(_data);
+            _viewModel = new ColonyViewModel(_data, _resources);
+        
+            // first wave launch
+            var waveManager = new WaveSpawner();
+            var cancellationToken = this.GetCancellationTokenOnDestroy();
+            waveManager.StartWaveAsync(initialWave, cancellationToken).Forget();
+        }
+
+        private void OnDestroy()
+        {
+            _viewModel?.Dispose();
+        }
+    }
+}
