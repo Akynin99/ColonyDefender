9e11ad05547a428118326378066a1b1e31ad46a7
Stepan Ustinov <Akynin99@gmail.com>
2025-04-21 11:21:33 +0700

    feat: add core game models

---
+++ Assets/Scripts/ColonyData.cs
@@ -0,0 +1,12 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace ColonyDefender
+{
+    public class ColonyData
+    {
+        public ReactiveProperty<int> Energy { get; } = new(1000);
+        public ReactiveProperty<int> Minerals { get; } = new(500);
+        public ReactiveProperty<int> Population { get; } = new(10);
+    }
+}
---
+++ Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace ColonyDefender
+{
+    [CreateAssetMenu(fileName = "WaveConfig", menuName = "ColonyDefender/WaveConfig", order = 0)]
+    public class WaveConfig : ScriptableObject
+    {
+        [SerializeField] private GameObject[] enemies;
+        [SerializeField] private float spawnInterval;
+        [SerializeField] private int bossActivationThreshold = 20;
+        
+        public GameObject[] Enemies => enemies;
+        public float SpawnInterval => spawnInterval;
+        public int BossActivationThreshold => bossActivationThreshold;
+    }
+}
