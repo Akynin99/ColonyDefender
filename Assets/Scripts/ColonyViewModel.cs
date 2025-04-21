0715628af223c430b1664e9ba3cfb271610fe56d
Stepan Ustinov <Akynin99@gmail.com>
2025-04-21 12:24:47 +0700

    feat: add ColonyViewModel

---
+++ Assets/Scripts/ColonyViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UniRx;
+
+namespace ColonyDefender
+{
+    public class ColonyViewModel : IDisposable
+    {
+        private readonly ColonyData _data;
+        private readonly IResourceService _resources;
+        private readonly CompositeDisposable _disposables = new();
+
+        public ColonyViewModel(ColonyData data, IResourceService resources)
+        {
+            _data = data;
+            _resources = resources;
+        
+            MessageBroker.Default.Receive<BuildingClicked>()
+                .Subscribe(OnBuildingClicked)
+                .AddTo(_disposables);
+        }
+
+        private void OnBuildingClicked(BuildingClicked evt)
+        {
+            if (_resources.TrySpendEnergy(evt.EnergyCost))
+            {
+                //TODO: add building logic
+            }
+        }
+
+        public void Dispose() => _disposables.Dispose();
+    }
+
+}
